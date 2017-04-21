6cd38d0295206da8a167d7d6424adccf15a8ac9a
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2017-04-20 22:42:23 -0500

    16/04/2017 17

--- sql/sql_api.js
+++ sql/sql_api.js
@@ -6,7 +6,7 @@ module.exports = {
     query_insertSensor: "INSERT INTO TBL_SENSOR (SERIAL_SENSOR, PK_NETWORK, NAME_SENSOR, STATUS_SENSOR, REGISTERDATE_SENSOR, UPDATEDATE_SENSOR) VALUES ('${serial}', ${network}, '${name}', 'Activo', '${date1}', '${date2}')",
     query_updateSensor: "UPDATE TBL_SENSOR SET STATUS_SENSOR = 'Inactivo', UPDATEDATE_SENSOR = '${date}' WHERE PK_SENSOR = ${pk_sensor}",
     query_verifyUploadPath: "SELECT count(*) AS counter FROM TBL_LOCATION WHERE UPLOADPATH_LOCATION = '${uploadPath}'",
-    query_verifyRegisterLocation: "SELECT count(*) AS counter FROM TBL_LOCATION WHERE PK_SENSOR = ${pk_sensor}",
+    query_verifyRegisterLocation: "SELECT count(*) AS counter FROM TBL_LOCATION WHERE PK_SENSOR = ${pk_sensor} AND STATUS_LOCATION = 'Activa'",
     query_getUploadPathNetwork: "SELECT UPLOADPATH_NETWORK FROM TBL_SENSOR INNER JOIN TBL_NETWORK ON TBL_SENSOR.PK_NETWORK = TBL_NETWORK.PK_NETWORK WHERE PK_SENSOR = ${pk_sensor}",
     query_insertLocation: "INSERT INTO TBL_LOCATION (PK_SENSOR, LAT_LOCATION, LNG_LOCATION, ALT_LOCATION, ADDRESS_LOCATION, REGISTERDATE_LOCATION, UPDATEDATE_LOCATION, UPLOADPATH_LOCATION, STATUS_LOCATION) VALUES (${pk_sensor},'${lat}','${lng}','${alt}','${address}','${date1}','${date2}','${uploadpath}','Activa')",
     query_insertFile: "INSERT INTO TBL_FILE (PK_SENSOR, PATH_FILE, DATE_FILE, HOUR_FILE, REGISTERDATE_FILE, PK_LOCATION, AXIS_FILE, TYPE_FILE) VALUES (${pk_sensor},'${path_file}', '${date}', '${hour}', '${reg_date}', ${pk_location}, '${axis}', '${type}')",
