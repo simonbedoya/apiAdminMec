5cbbb49e676048da112c8f86a6bc247749694946
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2017-04-23 15:38:47 -0500

    16/04/2017 17

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(bodyParser({limit: '50mb'}));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb'}));
 app.use(cookieParser());
